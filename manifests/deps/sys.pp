3af67b87a1103818ec53f474addd96070580950d
Alex Pica <dtk077@gmail.com>
2014-02-26 16:12:30 +0200

    Update the phalconphp::deps::sys class, and initial attempt to make the Modile work on debian

--- manifests/deps/sys.pp
+++ manifests/deps/sys.pp
@@ -1,55 +1,47 @@
 # Class: phalconphp::deps::sys
 # Installs gcc, make, automake, autoconf, re2c, pcre, pcre-devel, libcurl, libcurl-devel, wget
 class phalconphp::deps::sys {
-  if defined(Package['gcc']) == false {
-    package { 'gcc': ensure => present }
+  case $::osfamily {
+    'redhat' : { # do something RHEL specific
+      $packages = [
+        'gcc',
+        'git',
+        'autoconf',
+        'make',
+        'automake',
+        're2c',
+        'pcre',
+        'pcre-devel',
+        'openssl',
+        'openssl-devel',
+        'libcurl',
+        'libcurl-devel',
+        'wget']
+    }
+    'debian' : { # Install the debian
+      $packages = [
+        'git',
+        'gcc',
+        'make',
+        're2c',
+        'libpcre3-dev',
+        'openssl',
+        'libssl-dev',
+        'wget',
+        'curl',
+        'libcurl4',
+        'libcurl4-dev',
+        'libcurl4-openssl-dev']
+    }
+    default  : { #...
+      fail('Unknown Operating System')
+    }
   }
 
-  if defined(Package['git']) == false {
-    package { 'git': ensure => present }
-  }
-
-  if defined(Package['make']) == false {
-    package { 'make': ensure => present }
-  }
-
-  if defined(Package['automake']) == false {
-    package { 'automake': ensure => present }
-  }
-
-  if defined(Package['autoconf']) == false {
-    package { 'autoconf': ensure => present }
-  }
-
-  if defined(Package['re2c']) == false {
-    package { 're2c': ensure => present }
-  }
-
-  if defined(Package['pcre']) == false {
-    package { 'pcre': ensure => present }
-  }
-
-  if defined(Package['pcre-devel']) == false {
-    package { 'pcre-devel': ensure => present }
-  }
-
-  if defined(Package['openssl']) == false {
-    package { 'openssl': ensure => present }
-  }
-
-  if defined(Package['openssl-devel']) == false {
-    package { 'openssl-devel': ensure => present }
-  }
-
-  if defined(Package['libcurl']) == false {
-    package { 'libcurl': ensure => present }
-  }
-
-  if defined(Package['libcurl-devel']) == false {
-    package { 'libcurl-devel': ensure => present }
-  }
-
-  if defined(Package['wget']) == false {
-    package { 'wget': ensure => present }
+  each($packages) |$key, $package| {
+    if defined(Package[$package]) == false {
+      package { $package: ensure => present }
+    }
   }
 }
+
